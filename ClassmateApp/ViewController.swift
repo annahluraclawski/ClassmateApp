5e9765ffd3e21f32a5b0382b883ee0e4e46007ad
Annahlu Raclawski <annahlu47@gmail.com>
2023-10-11 09:16:16 -0500

    Initial Commit

---
+++ ClassmateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClassmateApp
+//
+//  Created by ANNAHLU RACLAWSKI on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
